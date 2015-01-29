278456f4477bcaaa19e1d78a16615eeb1b906f54
Adam Dingwell <publicadam2011@gmail.com>
2015-01-29 17:39:05 +0100

    Added extract_fields
    Started to re-write this set of tools for use with the offical FLEXPART-WRF branch
    The aim is to strip down WRF-output files to little more than what is needed by
    FLEXPART-WRF and compress the data using the netcdf4 deflation feature.

---
+++ extract_fields.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Extract fields for use in WRF and store in archive
+# Usage:
+#   extract_fields file1 [file2[,...]] output-directory
+
+# Set deflation level:
+export DFL_LVL=1   # 0=no compression, 9=max; higher than is rather pointless
+# Variables used by WRF
+VAR2D=ZNU,ZNW
+VAR3D=XLAT,XLONG,MAPFAC_M,PSFC,U10,V10,T2,Q2,SWDOWN,RAINNC,RAINC,HFX,UST
+VAR4D=PB,P,PBLH,PHB,PHB,T,QVAPOR,TKE
+VAR4D_WIND=U,V,W,WW
+# Additional variables (for further analysis or compatibility)
+VAR_EXTRAS=SR
+export VARS="$VAR2D,$VAR3D,$VAR4D,$VAR4D_WIND,$VAR_EXTRAS"
+
+# Precipitation fields
+# These fields are not normally read by FLX-WRF but have to be included
+# if cloud physics schemes output to them, otherwise precipitation will
+# be under estimated.
+PRECIP_C=RAINSH
+PRECIP_NC=SNOWNC,GRAUPELNC,HAILNC
+# WARNING: SNOWC is snow _cover_ (not "accumulated convective snow precip")
+
+# Read input arguments:
+ARGUMENTS=( $@ )
+NARGIN=${#ARRAY[@]}
+OUT_DIR=${ARRAY[$NARGIN-1]}
+FILES=${ARRAY[@]:0:$NARGIN-1}
+
+# Functions:
+copy_vars(){
+    local INFILE=$1
+    local OUTFILE=$2
+    ncks -O -L"$DFL_LVL" -v"$VARS" "$INFILE" "$OUTFILE"
+}
+
+
+#MAIN#
+
+
+COUNT=0
+for i in $FILES; do
+    let COUNT=COUNT+1
+    echo -e "FILE $COUNT:\t$i"
+done
